33027aad5921a7a971c89a3ec8851d2cde3b68ca
XMON765 <xavierm219@nycstudents.net>
2025-02-27 18:07:21 +0000

    update project

---
+++ script.js
@@ -0,0 +1,183 @@
+let scene, shield, shottyf, rnd = (l,u) => Math.random()*(u-l) + l, heal2, camera, enemy,  num, dnum, ui2, ui, health = 100, shieldHealth = 250, shieldDead = false, on = true, block = false, x = Math.round(health), y = Math.round(shieldHealth), enemies = [];
+let model, hitbox, floorbutton1, floorbutton2; 
+window.onload = function(){
+	
+	scene = document.querySelector("a-scene");
+	camera = document.getElementById("cam1");
+	heal = document.getElementById("heal");
+	ui = document.getElementById("ui");
+	ui2 = document.getElementById("ui2");
+	cam1 = document.getElementById("cam1");
+	cam2 = document.getElementById("cam2")
+	
+	ui.setAttribute("value", x);
+	ui2.setAttribute("value", shieldHealth);
+	ui2.setAttribute("position", "3.5 1 -2");
+	
+	hitbox = document.createElement("a-box");
+	model = document.createElement("a-gltf-model")
+	
+	floorbutton1 = document.getElementById("floorButton1");
+	floorbutton2 = document.getElementById("floorButton2");
+	
+	floorbutton1.addEventListener("click",function(){
+		cam1.setAttribute("active","true");
+		cam2.setAttribute("active","false");
+	})
+	
+	floorbutton2.addEventListener("click",function(){
+		cam1.setAttribute("active","true");
+		cam2.setAttribute("active","false");
+	})
+	
+	for(let i = 0; i < 10; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+	enemies.push(new Enemy(x,100,z));
+	}
+	//enemy = new Enemy(0, 0, -2)
+	heal2 = new Heal(3, 2, 5, "green");
+	heal3 = new Heal(5, 2, 5, "blue");
+	shield = new Shield;
+	
+	num = 0;
+	dnum = 1;
+	
+	window.addEventListener("keypress", function(e){
+		if(e.key == "e" && !_open){
+			_open = true;
+			dt = -dt;
+		}
+		
+	})
+	
+
+	
+	
+	
+	//shield = new Shield;
+	shotty = new Shotty;
+	//rifle = new Rifle;
+	
+	
+	window.addEventListener("keypress", function(e){
+		if(e.key == "o"){
+			cam1.setAttribute("active","false");
+			cam2.setAttribute("active","true");
+		}
+	})
+	
+	
+	window.addEventListener("keypress", function(e){
+		if(e.key == "e" && !_open){
+			_open = true;
+			dt = -dt;
+		}
+	})
+	
+	window.addEventListener("keypress", function(e){
+		if(e.key == "1"){
+			shottyf = true;
+			console.log(shottyf);
+		}
+	})
+	
+	window.addEventListener("keypress", function(e){
+		if(e.key == "2"){
+			shottyf = false;
+			console.log(shottyf);
+		}
+	})
+	
+	
+	window.addEventListener("click", ()=>{
+		shotty.shoot();
+		//rifle.shoot();
+	})
+	
+
+	setTimeout(loop, 1000);
+
+}
+
+let tmp=0, dt = -1; _open = false;
+
+function loop(){
+	
+	hitbox.setAttribute("position","0 5 1");
+	model.setAttribute("src", "#520a");
+	model.setAttribute("position","0 5 1");
+	
+	heal2.healing(10);
+	heal3.healShield(200);
+	
+	
+	shotty.update();
+	
+	
+	if(shieldHealth == 0){
+		shieldDead = true;
+	}
+	
+	
+	//Enemies functions
+	for(let enemy of enemies){
+		if(distance(enemy.enemy,cam2)<2.5 && block && !enemy.dead && shieldHealth>0){
+			shieldHealth -= 0.1
+		}
+		if(distance(enemy.enemy,cam2)<2.5 && !block && !enemy.dead){
+			enemy.attackPlayer();
+		}
+		if(distance(enemy.enemy,cam2)<2.5 && shieldHealth<0){
+			enemy.attackPlayer();
+			shield.unrotate();
+			_open = false;
+			block = false;
+			
+		}
+		
+		let d = distance(enemy.enemy, cam2);
+		if(2.5 < d && d < 60 && !enemy.dead){
+		enemy.rotateTowards(cam2); 
+		enemy.forward()
+		}
+		
+		if(enemy.enemyHealth == 0){
+			enemy.die();
+			enemy.dead = true;
+		}
+	}
+	
+	//Shield open and closing
+	if(_open){
+		tmp+=dt;
+		if(tmp >= 1){
+			_open = false;
+			block = true;
+			shield.rotate();
+		}
+		if(tmp <= 0){
+			_open = false;
+			block = false;
+			shield.unrotate();
+		}
+	}
+	
+	
+	ui.setAttribute("value", health);
+	ui2.setAttribute("value", shieldHealth);
+	
+	window.requestAnimationFrame( loop );
+}
+
+function distance(obj1,obj2){
+  let x1 = obj1.object3D.position.x;
+  let y1 = obj1.object3D.position.y;
+  let z1 = obj1.object3D.position.z;
+  let x2 = obj2.object3D.position.x;
+  let y2 = obj2.object3D.position.y;
+  let z2 = obj2.object3D.position.z;
+ 
+  let d = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2) + Math.pow(z1-z2,2));
+  return d;
+}
